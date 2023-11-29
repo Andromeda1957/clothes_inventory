dfc62f37882b6ae6cee8f0197f8a29f9dea40668
andromeda1957 <matrix0010101@gmail.com>
2023-11-29 20:24:18 +0000

    added delete functionality

--- wardrobe.py
+++ wardrobe.py
@@ -1,32 +1,25 @@
+import sys
 import sqlalchemy
 import dbconnect
-import sys
 
-def get_shirts(engine):
-    query = 'SELECT * FROM shirts'
-
-    with engine.connect() as sql:
-        shirts = sql.execute(sqlalchemy.text(query))
-
-    return shirts
-
-def get_pants(engine):
-    query = 'SELECT * FROM pants'
+def get_items(engine, item):
+    query = f'SELECT * FROM {item}'
 
     with engine.connect() as sql:
-        pants = sql.execute(sqlalchemy.text(query))
+        items = sql.execute(sqlalchemy.text(query))
 
-    return pants
+    return items
 
-def add_shirts(engine, shirt_id, shirt_type):
-    query = f"INSERT INTO shirts VALUES ('{shirt_id}', '{shirt_type}');"
+def add_item(engine, table, item_id, item_type):
+    query = f"INSERT INTO {table} VALUES ('{item_id}', '{item_type}');"
     
     with engine.connect() as sql:
         sql.execute(sqlalchemy.text(query))
         sql.commit()
 
-def add_pants(engine, pant_id, pant_type):
-    query = f"INSERT INTO pants VALUES ('{pant_id}', '{pant_type}');"
+def delete_item(engine, table, item_id):
+    query = f"DELETE FROM {table} WHERE id={item_id}"
+
     with engine.connect() as sql:
         sql.execute(sqlalchemy.text(query))
         sql.commit()
@@ -34,28 +27,44 @@ def add_pants(engine, pant_id, pant_type):
 def list_shirts(shirts):
     print('\n-------------------------------------------------------')
     print('Shirts:')
+
     for shirt in shirts:
         print(f'Shirt id: {shirt[0]} Shirt type: {shirt[1]}')
 
+    print('-------------------------------------------------------\n')
+
 def list_pants(pants):
-    print('\nPants:')
+    print('\n-------------------------------------------------------')
+    print('Pants:')
+
     for pant in pants:
         print(f'Pant id: {pant[0]} Pant type: {pant[1]}')
+
     print('-------------------------------------------------------\n')
+
+def help_menu():
+    pass
 
 def main():
     engine = dbconnect.connect()
-    shirts = get_shirts(engine)
-    pants = get_pants(engine)
+    shirts = get_items(engine, 'shirts')
+    pants = get_items(engine, 'pants')
     
-    #add_shirts(engine, '1', 'tshirt')
-    #add_pants(engine, '1', 'jeans')
-    list_shirts(shirts)
-    list_pants(pants)
+    if sys.argv[1] == 'addshirt':
+        add_item(engine, 'shirts', sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'addpants':
+        add_item(engine, 'pants', sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'listshirts':
+        list_shirts(shirts)
+    elif sys.argv[1] == 'listpants':
+        list_pants(pants)
+    elif sys.argv[1] == 'deleteshirt':
+        delete_item(engine, 'shirts', sys.argv[2])
+    elif sys.argv[1] == 'deletepants':
+        delete_item(engine, 'pants', sys.argv[2])
+    else:
+        help_menu()
 
-
-    
-    
 
 if __name__ == '__main__':
     main()
